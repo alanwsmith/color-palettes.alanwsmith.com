5a0e99b2aa21e1a669be8a18074306b235725803
Alan Smith <commit@alanwsmith.com>
2022-11-16 19:20:49 -0600

    highlighting the current pick

--- builder/src/JS.js
+++ builder/src/JS.js
@@ -20,64 +20,6 @@ const state = {
     arrangement: 0,
 }
 
-// let currentColors = []
-// let currentPalette = 0
-// let currentArrangement = 0
-
-// const changeColorsToIndex = (index) => {
-//     currentPalette = index
-//     currentArrangement = 0
-//     const values = palettes[index]
-//     const colors = values.colors
-//     document.body.style.backgroundColor = colors[0]
-//     document.body.style.color = colors[3]
-//     document.querySelectorAll('h1').forEach((a_tag) => {
-//         a_tag.style.color = colors[2]
-//     })
-//     document.querySelectorAll('h2').forEach((a_tag) => {
-//         a_tag.style.color = colors[2]
-//     })
-//     document.querySelectorAll('a').forEach((a_tag) => {
-//         a_tag.style.color = colors[1]
-//     })
-//     currentColors = permutator(colors)
-//     for (pi = 0; pi < currentColors.length; pi++) {
-//         for (si = 0; si < 4; si++) {
-//             document.getElementById(
-//                 `swatch--${pi}--${si}`
-//             ).style.backgroundColor = currentColors[pi][si]
-//         }
-//     }
-// }
-
-// const doChange = (event) => {
-//     changeColorsToIndex(parseInt(event.target.value, 10))
-// }
-
-// const changeArrangement = (index) => {
-//     document.body.style.backgroundColor = currentColors[index][0]
-//     document.body.style.color = currentColors[index][3]
-//     document.querySelectorAll('a').forEach((a_tag) => {
-//         a_tag.style.color = currentColors[index][2]
-//     })
-//     document.querySelectorAll('h1').forEach((a_tag) => {
-//         a_tag.style.color = currentColors[index][1]
-//     })
-//     document.querySelectorAll('h2').forEach((a_tag) => {
-//         a_tag.style.color = currentColors[index][1]
-//     })
-// }
-
-// const handleClick_old = (event) => {
-//     const paletteIndex = event.target.dataset.paletteIndex
-//     const colorArrangement = event.target.dataset.colorArrangement
-//     if (paletteIndex) {
-//         changeColorsToIndex(parseInt(paletteIndex, 10))
-//     } else if (colorArrangement) {
-//         changeArrangement(colorArrangement)
-//     }
-// }
-
 const handleClick = (event) => {
     const checkPalette = event.target.dataset.palette
     const checkArrangement = event.target.dataset.arrangement
@@ -88,18 +30,11 @@ const handleClick = (event) => {
         state.arrangement = parseInt(checkArrangement, 10)
     }
     updateColors()
-
-    // const paletteIndex = event.target.dataset.paletteIndex
-    // const colorArrangement = event.target.dataset.colorArrangement
-    // if (paletteIndex) {
-    //     changeColorsToIndex(parseInt(paletteIndex, 10))
-    // } else if (colorArrangement) {
-    //     changeArrangement(colorArrangement)
-    // }
 }
 
 const handleKeydown = (event) => {
     const theKey = event.key.toLowerCase()
+    // console.log(theKey)
     if (theKey === 'arrowdown') {
         if (state.palette < palettes.length - 2) {
             state.palette += 1
@@ -127,26 +62,10 @@ const handleKeydown = (event) => {
             updateColors()
         }
     }
-
-    // } else if (theKey === 'k') {
-    //     if (currentPalette > 0) {
-    //         currentPalette -= 1
-    //         changeColorsToIndex(currentPalette)
-    //     }
-    // } else if (theKey === 'l') {
-    //     if (currentArrangement < 24) {
-    //         currentArrangement += 1
-    //         changeArrangement(currentArrangement)
-    //     }
-    // } else if (theKey === 'h') {
-    //     if (currentArrangement > 0) {
-    //         currentArrangement -= 1
-    //         changeArrangement(currentArrangement)
-    //     }
-    // }
 }
 
 const updateColors = () => {
+    // console.log(state)
     // Get the colors and put them in the display order
     const rawColorSet = permutator(palettes[state.palette].colors)
     const colorSet = [
@@ -175,7 +94,6 @@ const updateColors = () => {
         rawColorSet[16],
         rawColorSet[13],
     ]
-
     document.body.style.backgroundColor = colorSet[state.arrangement][0]
     document.body.style.color = colorSet[state.arrangement][3]
     document.querySelectorAll('h1').forEach((el) => {
@@ -184,7 +102,6 @@ const updateColors = () => {
     document.querySelectorAll('a').forEach((el) => {
         el.style.color = colorSet[state.arrangement][2]
     })
-
     for (pi = 0; pi < colorSet.length; pi++) {
         for (si = 0; si < 4; si++) {
             document.getElementById(
@@ -196,9 +113,17 @@ const updateColors = () => {
         palettes[state.palette].name
     }-${state.arrangement}`
 
-    document
-        .getElementById(`palette-wrapper-${state.palette}`)
-        .scrollIntoView({ behavior: 'smooth', block: 'nearest' })
+    document.querySelectorAll('.palette-wrapper').forEach((el) => {
+        el.classList.remove('active-palette')
+        el.classList.add('inactive-palette')
+    })
+
+    const activePalette = document.getElementById(
+        `palette-wrapper-${state.palette}`
+    )
+    activePalette.scrollIntoView({ behavior: 'smooth', block: 'nearest' })
+    activePalette.classList.remove('inactive-palette')
+    activePalette.classList.add('active-palette')
 }
 
 const init = () => {
